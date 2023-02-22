aac071f7c9c12490a9a5d6525311df7d5409142a
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2023-02-23 08:12:14 +0900

    namespaceについて学習

--- src/namespaces.ts
+++ src/namespaces.ts
@@ -1,33 +1,33 @@
 export {};
 
 namespace Japanese {
-  export namespace Tokyo {
-    export class Person {
-      constructor(public name: string) {}
-    }
-  }
-  export namespace Osaka {
-    export class Person {
-      constructor(public name: string) {}
-    }
-  }
+	export namespace Tokyo {
+		export class Person {
+			constructor(public lastName: string, public firstName: string) {
+			}
+		}
+	}
+
+	export namespace Osaka {
+		export class Person {
+			constructor(public lastName: string, public firstName: string) {
+			}
+		}
+	}
 }
 
 namespace English {
-  export class Person {
-    constructor(
-      public firstName: string,
-      public middleName: string,
-      public lastName: string
-    ) {}
-  }
+	export class Person {
+		constructor(public lastName: string, public firstName: string, public middleName: string) {
+		}
+	}
 }
 
-const me = new Japanese.Tokyo.Person('はむさん');
-console.log(me.name);
+const me = new Japanese.Osaka.Person('山田', '太郎');
+console.log(me.lastName + ' ' + me.firstName);
 
-const meOsaka = new Japanese.Osaka.Person('はむやん');
-console.log(meOsaka.name);
+const you = new Japanese.Tokyo.Person('山田', '花子');
+console.log(you.lastName + ' ' + you.firstName);
 
-const michael = new English.Person('Michael', 'Joseph', 'Jackson');
-console.log(michael);
+const michael = new English.Person('Jackson', 'Michael', 'Joseph');
+console.log(michael.firstName + ' ' + michael.middleName + ' ' + michael.lastName + ' ' + "< Poh!");
