50d6613f80a9288bfa73e178fe0118ddef6765df
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2023-02-23 07:30:51 +0900

    getterの作成

--- src/my-first-class.ts
+++ src/my-first-class.ts
@@ -1,11 +1,22 @@
 export{};
 class Person{
-    private name: string;
-    private age: number
+    private readonly name: string;
+    private readonly age: number
     constructor(name: string, age: number){
         this.name = name;
         this.age =age;
     }
+
+    // publicは省略可能
+    public getName(): string{
+        return this.name;
+    }
+
+    // 上と同じ
+    getAge(): number{
+        return this.age;
+    }
+
 }
 
 let taro = new Person("太郎", 29);
