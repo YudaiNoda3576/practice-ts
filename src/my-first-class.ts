32aff869e6adbb62ff87be5afbe61b9827f6599a
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2023-02-23 07:29:18 +0900

    アクセス修飾子を追加

--- src/my-first-class.ts
+++ src/my-first-class.ts
@@ -1,18 +1,12 @@
-export {};
-
-class Person {
-  name: string;
-  age: number;
-
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
-  }
-
-  profile(): string {
-    return `name: ${this.name}, age: ${this.age}`;
-  }
+export{};
+class Person{
+    private name: string;
+    private age: number
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age =age;
+    }
 }
-let taro = new Person('Taro', 30);
-console.log(taro.profile());
-// let hanako = new Person();
+
+let taro = new Person("太郎", 29);
+console.log(taro);
